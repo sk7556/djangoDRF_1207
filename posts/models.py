f021214e6bd5d795000f702b8b44e6388c91eef7
Jungsik <sk7556@naver.com>
2023-12-15 21:24:10 +0900

    ✅

--- posts/models.py
+++ posts/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
         return f'{self.author} - {self.pk}'
     
 
-class Comment(models.Model): 
+class Comment(models.Model):  
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
