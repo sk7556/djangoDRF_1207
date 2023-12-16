7119401cd8c6c6f22ea4428e68abaa915f600ad5
Jungsik <sk7556@naver.com>
2023-12-16 20:19:00 +0900

    🐛

--- posts/models.py
+++ posts/models.py
@@ -16,7 +16,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)  
 
     def __str__(self):
         return f'{self.author} - {self.post} - {self.pk}'
